f370f2ae03c174e25d3849449867e7986bbc115e
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-10-26 21:28:42 -0500

    prueba de conexion websocket 3

--- client.js
+++ client.js
@@ -1,19 +1,12 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://api-onepiece-6m7v.onrender.com');
+const io = require('socket.io-client');
+const socket = io('https://api-onepiece-6m7v.onrender.com');
 
-ws.on('open', () => {
-    console.log('Conexión WebSocket establecida');
-    ws.send('Mensaje de prueba');
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+    socket.emit('mensaje', 'Hola desde el cliente');
 });
 
-ws.on('message', (data) => {
-    console.log('Mensaje recibido del servidor:', data);
-});
-
-ws.on('error', (error) => {
-    console.error('Error en WebSocket:', error);
-});
-
-ws.on('close', () => {
-    console.log('Conexión WebSocket cerrada');
+socket.on('respuesta', (data) => {
+    console.log('Respuesta del servidor:', data);
+    socket.disconnect();
 });
